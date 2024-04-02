f1dae5cd21edfa87d8710be3da64a4258c3f52d0
John Lopez <john.lopez3695@gmail.com>
2024-04-02 16:27:21 -0400

    Completed edit,create,delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import Home
+from .views import Home, PianoList,PianoDetail
 
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('pianos/', PianoList.as_view(), name='Piano-list'),
+    path('pianos/<int:id>/', PianoDetail.as_view(), name='Piano-detail'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import Piano
+from .serializers import PianoSerializer
 
 # Create your views here.
 
@@ -8,3 +11,12 @@ class Home(APIView):
     def get(self, request):
         content = {'message': 'Welcome to the cat-collector api home route!'}
         return Response(content)
+
+class PianoList(generics.ListCreateAPIView):
+  queryset = Piano.objects.all()
+  serializer_class = PianoSerializer
+
+class PianoDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Piano.objects.all()
+  serializer_class = PianoSerializer
+  lookup_field = 'id'
