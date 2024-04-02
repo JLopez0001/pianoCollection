49a9aef695c053e76ff5f64a826ac87065183907
John Lopez <john.lopez3695@gmail.com>
2024-04-02 11:48:20 -0400

    Finished lab 1

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 # Create your views here.
 
 # Define the home view
-
 class Home(APIView):
     def get(self, request):
         content = {'message': 'Welcome to the cat-collector api home route!'}
