4e36a4ca687bd30eec71fc63438506b87f8a327f
John Lopez <john.lopez3695@gmail.com>
2024-04-03 13:34:00 -0400

    added maintenance inital logic

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Piano
+from .models import Piano, MaintenanceRecord
 
 class PianoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Piano
         fields = '__all__'
+
+class MaintenanceRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MaintenanceRecord
+        fields = '__all__'
+        read_only_fields = ('piano',)
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('pianos/', PianoList.as_view(), name='Piano-list'),
     path('pianos/<int:id>/', PianoDetail.as_view(), name='Piano-detail'),
-
+    path('pianos/<int:id>/maintenance/', MaintenanceListCreate.as.view(), name='maintenance-list-create'),
+    path('pianos/<int:id>/maintenance/<int:id>', MaintenanceDetail.as.view(), name='maintenance-detail')
 ]
